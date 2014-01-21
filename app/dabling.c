5912bad27edbc65d51cc2a150eaefb4f0a9e8141
Matthias Kleemann <dev@layer128.net>
2014-01-21 22:58:09 +0100

    create skeleton for the project
    - add submodules timer and adc
    - create cmake control files
    - add doxygen configuration file
    - add empty main loop

---
+++ app/dabling.c
@@ -0,0 +1,34 @@
+/**
+ * ----------------------------------------------------------------------------
+ *
+ * "THE ANY BEVERAGE-WARE LICENSE" (Revision 42 - based on beer-ware license):
+ * <dev@layer128.net> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a be(ve)er(age) in return. (I don't
+ * like beer much.)
+ *
+ * Matthias Kleemann
+ *
+ * ----------------------------------------------------------------------------
+ *
+ * \file dabling.c
+ *
+ * \date Created: 21.01.2014 22:55:56
+ * \author Matthias Kleemann
+ **/
+
+// === MAIN LOOP =============================================================
+
+/**
+ * \brief main loop
+ *
+ **/
+int __attribute__((OS_main)) main(void)
+{
+   while(1)
+   {
+      // do nothing...yet!
+   }
+}
+
+
